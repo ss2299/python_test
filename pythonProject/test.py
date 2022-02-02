f6d996c191def33b9def8a044b0335801b57d8aa
ss2299 <ss2299@hotmail.com>
2022-02-02 22:53:06 +0800

    Commit in PyCharm

---
+++ pythonProject/test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+# from selenium.webdriver.firefox.service import Service
+# from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import ActionChains
+import pandas as pd
+import time
+
