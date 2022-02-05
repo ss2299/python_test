ea8dd91350a78b7954dfbfd6d5fd74171f8a3732
ss2299 <ss2299@hotmail.com>
2022-02-05 23:32:22 +0800

    Commit in PyCharm

--- PyCharm/Serial.py
+++ PyCharm/Serial.py
@@ -1,8 +1,14 @@
 import serial
 from serial.tools import list_ports
 
+
+
 ports_list = list_ports.comports()
-port = '/dev/cu.usbserial-AG0K5F4O'
+# ports_list:
+
+
+
+# port = '/dev/cu.usbserial-AG0K5F4O'
 baud = 9600             # 시리얼 보드레이트(통신속도)
 
 ser = serial.Serial(port, baud, timeout=1)
